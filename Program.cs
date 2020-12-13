e0f0902461f508cfc769caf7b8790a2bb3f95b87
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-13 16:10:53 +0200

    example of abstract method and his children classes

--- Program.cs
+++ Program.cs
@@ -5,32 +5,59 @@ using System.Collections.Generic;
 namespace _8.Polymorhism
 {
 
-    class Shape 
+    abstract class Shape
     {
-    
+        public abstract double CalcArea();
     }
     class Rect : Shape
     {
-       public string Name { get; set; }
-    }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
 
+
+        public Rect(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public override double CalcArea()
+        {
+            return this.Width * this.Height;
+        }
+    }
+    // Interfaces implement kind of  multiple Inheritance, // child class can have many parents   
+    class Circle : Shape  // one class can inherits only from one another class / a child class can have only one parent-- SingleInheritance
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public override double CalcArea()
+        {
+            return this.Radius * this.Radius * Math.PI;
+        }
+
+    }
     interface IMovable    /// Interface !!!
     {
         void Move(int dx, int dy);
     }
 
-    class MovableRect : Rect, IMovable
-    {
-        public void Move(int dx, int dy)
-        {
-            Console.WriteLine("Rect Moved");
-        }
-    }
+    //class MovableRect : Rect, IMovable
+    //{
+    //    public void Move(int dx, int dy)
+    //    {
+    //        Console.WriteLine("Rect Moved");
+    //    }
+    //}
 
-    class Image 
+    class Image
     {
-    
-      
+
+
     }
     class MovableImage : Image, IMovable   // MovalbeImage implements Method Move() from Interface IMovable
     {
@@ -39,10 +66,10 @@ namespace _8.Polymorhism
             Console.WriteLine("Image moved");
         }
     }
-    public class Program
+    public class OopPrinciplesDemos
     {
-        
-        
+
+
         static void Print<T>(IEnumerable<T> items)
         {
 
@@ -55,16 +82,25 @@ namespace _8.Polymorhism
             Console.WriteLine(line);
         }
 
-        static void MoveObject(IMovable Obj) 
+        static void MoveObject(IMovable Obj)
         {
             Obj.Move(5, 1);
         }
         public static void Main()
         {
-            MoveObject(new MovableImage());
-            MoveObject(new MovableRect());
-            // MoveObject((IMovable)new Rect()); -> InvalidCastExcption ->  can not cast one type to another 
 
+            Shape[] shapes =
+            {
+
+                new Rect(5, 6),
+                new Circle(5)
+
+            };
+
+            foreach (Shape shape in shapes)            // We call the current object shape in the arrayShapes, we call it through 
+            {                                         // the Abstract class Shape , the  abstract Shape Class has a method CalcArea
+                Console.WriteLine(shape.CalcArea());
+            }
 
 
             //IMovable[] movables = 
@@ -80,15 +116,19 @@ namespace _8.Polymorhism
             //}
 
             // List<int> numbers = new List<int> {1, 2, 3 , 4 };  
-           // IEnumerable<int> numbers = new List<int> { 1, 2, 3, 4 }; // List inherits IEnumerable, 
+            // IEnumerable<int> numbers = new List<int> { 1, 2, 3, 4 }; // List inherits IEnumerable, 
             //thats why IEnumerable Object can keep a value of Oject List;
             // here is exampel of abstraction,  saying IEnumerable we say we will work with almost anithing that inherists from IEnumerable
             // List, Array , ......
-            
-            Print( new List<int> { 1 , 2 , 3 , 4});
-            Print( new int[] {1, 2, 3, 4 });
-            Print( new string[] { "one", "two"});
-           
+
+            //Print( new List<int> { 1 , 2 , 3 , 4});
+            //Print( new int[] {1, 2, 3, 4 });
+            //Print( new string[] { "one", "two"});
+
+            // Object of a type Parent  can keep , object values of a Type child .
+            // oposite,  Object of type child can not keep values of Object of a type parent.
+
+
         }
 
     }
